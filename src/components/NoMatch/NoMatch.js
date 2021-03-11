57a6257f0b532826b8fad6e0f3ecc3547581dd1e
omar faruq <omarfaruq.bplus.6600@gmail.com>
2021-03-11 20:08:25 +0600

    fivetimes

--- src/components/NoMatch/NoMatch.js
+++ src/components/NoMatch/NoMatch.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import './noMatch.css';
 
 const NoMatch = () => {
     return (
         <div>
-            <h1>404, not found......</h1>
+            <div class="vertical-center">
+		<div class="container">
+			<div id="notfound" class="text-center ">
+				<h1>😮</h1>
+				<h2>Oops! Page Not Be Found</h2>
+				<p>Sorry but the page you are looking for does not exist.</p>
+				<a href="#">Back to homepage</a>
+			</div>
+		</div>
+	</div>
         </div>
     );
 };
